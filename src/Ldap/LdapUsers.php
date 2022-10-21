174fdcf4a1f1a084222f26021fb521330648dd5f
b-thanh <b-thanh@plott.co.jp>
2022-10-21 14:49:09 +0700

    fix crawl data from modals

--- src/Ldap/LdapUsers.php
+++ src/Ldap/LdapUsers.php
@@ -3,9 +3,11 @@
 namespace BThanh\FirstPackage\Ldap;
 
 use LdapRecord\Models\Model;
+use LdapRecord\Models\ActiveDirectory\Entry;
 
 class LdapUsers extends Model
 {
+    protected $connection = 'AD';
     /**
      * The object classes of the LDAP model.
      *
